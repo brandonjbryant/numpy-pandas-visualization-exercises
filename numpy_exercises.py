86de98a7eaa49b1219c56f89840b3bfb3745de31
“Brandon <brandon.j.bryant1@gmail.com>
2021-01-21 11:47:01 -0600

    Added 3 to each data point

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -9,3 +9,9 @@ len(a[a > 0])
 
 #3.How many even positive numbers are there?
 len(a[(a > 0) & (a % 2 == 0)])
+
+#4.If you were to add 3 to each data point, how many positive numbers would there be?
+a_plus_three = a + 3
+a_plus_three 
+
+len(a_plus_three)
